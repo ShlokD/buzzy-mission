94a983785e617b3c1e0ab802f5de5626dc39d3c7
ShlokD <shlokpdesai@gmail.com>
2017-07-04 18:11:58 -0700

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+### Buzzing Js
+
+Random node module to generate buzzwordy mission statements
+
+List of buzzwords from http://ngenworks.com/culture/250-buzzwords-we-love-to-hate/
+List of verbs from https://www.englishclub.com/vocabulary/regular-verbs-list.htm
---
+++ index.js
@@ -0,0 +1,30 @@
+const verbsList = require('./verbs.js');
+const buzzWordList = require('./buzzwords.js');
+
+const generateRandomInt = (howMany) => Math.floor(Math.random() * howMany) + 1;
+
+const getVerbs = () => {
+  verbs = [];
+  for(let i = 0; i < 3; ++i) {
+    verbs.push(verbsList[generateRandomInt(verbsList.length - 1)].trim());
+  }
+  return verbs;
+};
+
+const getBuzzWords = () => {
+  buzzWords = [];
+  for(let i = 0; i < 5; ++i) {
+    const randomBuzzWord = buzzWordList[generateRandomInt(buzzWordList.length - 1)].trim();
+    buzzWords.push(randomBuzzWord);
+  }
+  return buzzWords;
+};
+
+const generate = () => {
+  const verbs = getVerbs();
+  const buzzwords = getBuzzWords();
+  const statement = `Our mission is to ${verbs[0]} ${buzzwords[0]} and ${verbs[1]} ${buzzwords[1]} in order to ${verbs[2]} ${buzzwords[3]} for ${buzzwords[4]}.`;
+  return statement.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+
+console.log(generate());
